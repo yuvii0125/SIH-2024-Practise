57e05e793fb1e00a690167fe476b7dacc882de4a
yuvii0125 <yuvrajmakwana100@outlook.com>
2024-08-27 10:50:15 +0530

    Update login.js

--- login.js
+++ login.js
@@ -37,3 +37,28 @@ function moveSlider() {
 bullets.forEach((bullet) => {
   bullet.addEventListener("click", moveSlider);
 });
+
+let zoomLevel = 1;
+        const zoomContainer = document.getElementById('zoomContainer');
+
+        function zoomIn() {
+            zoomLevel += 0.1;
+            zoomContainer.style.transform = `scale(${zoomLevel})`;
+        }
+
+        function zoomOut() {
+            if (zoomLevel > 0.1) {
+                zoomLevel -= 0.1;
+                zoomContainer.style.transform = `scale(${zoomLevel})`;
+            }
+        }
+
+        document.addEventListener('keydown', function(event) {
+            if (event.ctrlKey && event.key === '+') {
+                event.preventDefault();
+                zoomIn();
+            } else if (event.ctrlKey && event.key === '-') {
+                event.preventDefault();
+                zoomOut();
+            }
+        });
